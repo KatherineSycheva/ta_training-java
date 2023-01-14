f2af9b162e749977ef8a1eb8ca258d885adb2f9e
Ekaterina Sycheva <katherinesycheva@gmail.com>
2023-01-14 11:45:41 +0300

    Task "Go Dutch"

---
+++ src/main/java/com/epam/training/ekaterina_sycheva/java_basics/go_dutch/GoDutch.java
@@ -0,0 +1,22 @@
+package com.epam.training.ekaterina_sycheva.java_basics.go_dutch;
+
+import java.util.Scanner;
+
+public class GoDutch {
+
+    public static void main(String[] args) {
+        //Write code here
+        Scanner console = new Scanner(System.in);
+        int bill_sum = console.nextInt();
+        int friends = console.nextInt();
+
+        if (bill_sum < 0) {
+            System.out.println("Bill total amount cannot be negative");
+        } else if (friends <= 0) {
+            System.out.println("Number of friends cannot be negative or zero");
+        } else {
+            int part = (int) ((bill_sum + bill_sum * 0.1) / friends);
+            System.out.println(part);
+        }
+    }
+}
