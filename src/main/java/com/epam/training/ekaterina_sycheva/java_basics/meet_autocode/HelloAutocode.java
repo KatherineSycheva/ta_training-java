ce941c00a2e576a7e0b4156cd2a332d809f3cd15
Ekaterina Sycheva <katherinesycheva@gmail.com>
2023-01-15 17:58:14 +0300

    Task "Meet AutoCode"

---
+++ src/main/java/com/epam/training/ekaterina_sycheva/java_basics/meet_autocode/HelloAutocode.java
@@ -0,0 +1,8 @@
+package com.epam.training.ekaterina_sycheva.java_basics.meet_autocode;
+
+public class HelloAutocode {
+    public static void main(String[] args) {
+        //Write a program, printing "Hello, Autocode!"
+       System.out.println("Hello, Autocode!");
+    }
+}
